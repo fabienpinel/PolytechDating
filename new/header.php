8d49b288ea0684fe3428a93607a7d414f694f543
fabien <pinel.fabien@gmail.com>
2014-07-22 18:02:37 +0200

    fr

--- new/header.php
+++ new/header.php
@@ -5,7 +5,7 @@
   Site développé par Amir Ben Slimane et Fabien Pinel
 -->
 <!DOCTYPE html>
-<html lang="en"><head>
+<html lang="fr"><head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
