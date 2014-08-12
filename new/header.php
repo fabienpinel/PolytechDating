95481f5be7ef22f094ed32c8452fac64e7e89a52
fabien <pinel.fabien@gmail.com>
2014-08-12 10:34:30 +0200

    description, auteur dans header

--- new/header.php
+++ new/header.php
@@ -10,8 +10,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Polytech Dating">
+    <meta name="author" content="Fabien Pinel">
     <link rel="icon" type="image/png" href="/images/icon.ico" />
 
     <title>Polytech Dating 2014</title>
